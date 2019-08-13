73845339f57d8774d2ebb05b8c44b016612b7adc
Matthew Schuelke <matthew.schuelke@slu.edu>
2019-08-13 17:13:09 -0500

    add activity input

---
+++ R/activity_input.R
@@ -0,0 +1,36 @@
+#' Activity Input
+#'
+#' @name activity_input
+#'
+#' @description Retrieves activity input
+#'
+#' @param approval_id integer
+#' Specify the approval process to lookup.
+#' @param faculty_id integer
+#' Specify the facultyid of the user.
+#' @param formName string
+#' Specify the name of the form to lookup.
+#' @inheritParams fac_get
+#'
+#' @return a \code{\link[httr]{response}} object
+#'
+#' @examples
+#' ## need some examples
+#'
+#' @note API documentation \url{https://faculty180.interfolio.com/swagger/ui/#tag/Activity-Input}
+NULL
+
+#' @rdname activity_input
+#' @export
+fac_get_activity_input_approval_status <- function(approval_id,
+                                                   faculty_id,
+                                                   ...) {
+  fac_get(paste("/activity_input", approval_id, faculty_id, sep = "/"), ...)
+}
+
+#' @rdname activity_input
+#' @export
+fac_get_activity_input_form_name_units <- function(formName,
+                                                   ...) {
+  fac_get(paste0("/activity_input/form_name_units/", formName), ...)
+}
